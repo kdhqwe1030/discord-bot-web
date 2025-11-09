7c03787439c9fdf629be81d92ce296c83e8f2d56
kdhqwe1030 <kdhqwe1030@naver.com>
2025-11-09 15:44:49 +0900

    feat: 로그인 구현

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,29 @@
+// app/api/auth/login/route.ts
+import { createClient } from "@/lib/supabase/supabaseServer";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const supabase = await createClient();
+  const { email, password } = await req.json();
+
+  if (!email || !password) {
+    return NextResponse.json(
+      { error: "Email and password are required" },
+      { status: 400 }
+    );
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 401 });
+  }
+
+  return NextResponse.json({
+    message: "Login successful",
+    user: data.user,
+  });
+}
