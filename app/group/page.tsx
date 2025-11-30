c460a3eb4b54737f50a8cd98861adb888f9747b4
kdhqwe1030 <kdhqwe1030@naver.com>
2025-11-30 19:41:37 +0900

    feat: 그룹 페이지 레이아웃 구축

---
+++ app/group/page.tsx
@@ -0,0 +1,13 @@
+import GroupCardWrapper from "@/components/groups/GroupCardWrapper";
+
+const groupPage = () => {
+  return (
+    <div>
+      <div className="mt-6 p-6">
+        <GroupCardWrapper />
+      </div>
+    </div>
+  );
+};
+
+export default groupPage;
