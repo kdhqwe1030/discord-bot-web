51aca47daa262ac2d09108413be1f8a61d1f01f2
kdhqwe1030 <kdhqwe1030@naver.com>
2025-12-02 19:41:18 +0900

    fix: serverUrl 제거

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
