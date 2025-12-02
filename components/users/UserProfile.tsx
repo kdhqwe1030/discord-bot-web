1eba06aa36d93de967fefaf2a97f8f9b5fd0a572
kdhqwe1030 <kdhqwe1030@naver.com>
2025-12-02 22:09:33 +0900

    chore: 테두리 스타일 변경

--- components/users/UserProfile.tsx
+++ components/users/UserProfile.tsx
@@ -3,7 +3,7 @@ import { FaUser } from "react-icons/fa";
 
 const UserProfile = ({ imgUrl }: { imgUrl: string }) => {
   return (
-    <div className="w-8 h-8 flex items-center justify-center bg-surface-1  rounded-full overflow-hidden ring-2 ring-sub3">
+    <div className="w-8 h-8 flex items-center justify-center bg-surface-1 rounded-full overflow-hidden ">
       {imgUrl === "" ? (
         <FaUser className="text-text-3 " />
       ) : (
